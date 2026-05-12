afeb44ec5aaf4e991e94df846c5dd6a04075dd18
unknown <aldmyatyfrhh@gmail.com>
2026-05-12 18:43:55 +0300

    update cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: true,
+    credentials: true,
   });
 
   app.use(cookieParser());
